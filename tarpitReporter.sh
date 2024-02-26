e1d8c22aa62b8219daabdf5c280e89e27b966a40
@arbs09 <101005375+arbs09@users.noreply.github.com>
2024-02-26 23:06:27 +0100

    Update tarpitReporter.sh

--- tarpitReporter.sh
+++ tarpitReporter.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-endlessh -f endlessh-config-example.conf -v
-  grep 'ACCEPT' --line-buffered | \
-  while IFS= read -r line;
-   do ./report.sh "$line";
-  done
+endlessh -f endlessh-config-example.conf -v | grep 'ACCEPT' | while IFS= read -r line; do
+  ./report.sh "$line";
+done
